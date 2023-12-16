6bbb9f64ad46cebbec34da59b02a71dce089dffd
chenxi393 <1578477681@qq.com>
2023-12-17 03:45:33 +0800

    other student

---
+++ ahthorStudent.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+        cout<<"4.与项目组同学合作，各自将本地代码提到同一个远程版本库；"<<endl;
+        cout<<"202100801035 邓烨钒"<<endl;
+        cout<<"这是另一个同学提交的的内容"<<endl;
+        return 0;
+}
