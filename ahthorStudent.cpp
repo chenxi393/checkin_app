75fbfcc210fe4c38df867b087a2e1afaa8177154
chenxi393 <1578477681@qq.com>
2024-04-21 22:35:44 +0800

    Update ahthorStudent.cpp

--- ahthorStudent.cpp
+++ ahthorStudent.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 
         cout<<"4.与项目组同学合作，各自将本地代码提到同一个远程版本库；"<<endl;
-        cout<<"202100801035 邓烨钒"<<endl;
+        cout<<"测试 邓烨钒"<<endl;
         cout<<"这是另一个同学提交的的内容"<<endl;
         return 0;
 }
