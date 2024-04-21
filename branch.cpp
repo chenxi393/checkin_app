49e192ab305c879907278c2fce0bc00b83f82965
chenxi393 <1578477681@qq.com>
2024-04-21 23:00:38 +0800

    feat: test (#1)

--- branch.cpp
+++ branch.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 
 	cout<<"这是使用分支提交的代码"<<endl;
-	cout<<"202100801035 邓烨钒"<<endl;
+	cout<<"你好 邓烨钒"<<endl;
 	cout<<"这是分支branch1 修改的内容"<<endl;
 	return 0;
 }
--- main.cpp
+++ main.cpp
@@ -4,11 +4,6 @@ using namespace std;
 int main(){
 
 	cout<<"这是软件工程第四次实验"<<endl;
-	cout<<"202100801035 邓烨钒"<<endl;
-<<<<<<< HEAD
-	cout<<"模拟冲突解决 这是本人提交"<<endl;
-=======
-	cout<<"5.与项目组同学共同编写同一个代码文件，体验冲突解决；"<<endl;
->>>>>>> d3cbd9f9a56bb96db3190f0429082dfb33dba1bb
+	cout<<"测试 rebase"<<endl;
 	return 0;
 }
